0cfe71238d16ee99a00b292e64661f34a73c00e7
Ankit Pokhrel <ankitpokhrel@gmail.com>
2016-12-05 13:33:43 +0700

    Init gulp

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp'),
+  sass = require('gulp-sass'),
+  concat = require('gulp-concat'),
+  notify = require('gulp-notify');
+
+gulp.task('sass', function() {
+  return gulp.src('./resources/scss/app.scss')
+    .pipe(concat('wtr-styles.css'))
+    .pipe(sass().on('error', notify.onError({
+      message: 'Error: <%= error.message %>'
+    })))
+    .pipe(gulp.dest('./build/css/'));
+});
+
+gulp.task('default', ['sass'], function() {
+  gulp.watch('./app/resources/scss/app.scss', ['sass']);
+});
